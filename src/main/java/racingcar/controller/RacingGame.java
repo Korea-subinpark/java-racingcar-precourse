e3e13b727d4db051ec394e53bd733cdcea4587c0
kgnma <subinpark.re@gamil.com>
2022-04-22 23:06:42 +0900

    [feat] 잘못 입력시 다시 입력 받는 기능 구현

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -5,13 +5,7 @@ import racingcar.view.InputView;
 public class RacingGame {
 
     public void play() {
-        try {
-            String[] users = InputView.inputUsers();
-            int round = InputView.inputRound();
-        } catch (NumberFormatException e) {
-            System.out.println("[ERROR] 숫자가 아닌 문자가 입력되었습니다.");
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 이름이 5자를 초과했습니다.");
-        }
+        String[] users = InputView.inputUsers();
+        int round = InputView.inputRound();
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,18 +7,27 @@ public class InputView {
     private static final int USERNAME_LENGTH_LIMIT = 5;
 
     public static String[] inputUsers() {
-        System.out.println("-경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
-        String[] inputs = Console.readLine().split(",");
-        for (String input : inputs) {
-            validateUsername(input);
+        try {
+            System.out.println("-경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)");
+            String[] inputs = Console.readLine().split(",");
+            for (String input : inputs) {
+                validateUsername(input);
+            }
+            return inputs;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] 이름이 5자를 초과했습니다.");
+            return inputUsers();
         }
-
-        return inputs;
     }
 
     public static int inputRound() {
-        System.out.println("-시도할 횟수");
-        return Integer.parseInt(Console.readLine());
+        try {
+            System.out.println("-시도할 횟수");
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] 숫자가 아닌 문자가 입력되었습니다.");
+            return inputRound();
+        }
     }
 
     private static void validateUsername(String input) {
